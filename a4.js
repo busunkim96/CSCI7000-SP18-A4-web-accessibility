aa3377aa7b8ca1fc466003d1bd4e66b007942390
Bu Sun Kim <busunkim96@gmail.com>
2018-02-21 12:52:15 -0700

    initial commit

---
+++ a4.js
@@ -0,0 +1,11 @@
+$("#submit1").click(function(){
+  $("#alert1").show();
+})
+
+$("#submit2").click(function(){
+  $("#alert2").show();
+})
+
+$("#submit3").click(function(){
+  $("#alert3").show();
+})
